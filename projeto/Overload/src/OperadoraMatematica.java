9ea02a2859aa72b902e7d60943fbdaa097c48f6e
GUIGN-eng <guign1610@gmail.com>
2026-05-06 11:57:35 -0300

    Add files via upload

---
+++ projeto/Overload/src/OperadoraMatematica.java
@@ -0,0 +1,3 @@
+public interface OperadoraMatematica {
+    public int soma (int a , int b);
+}
---
+++ projeto/Overload/src/Soma.java
@@ -0,0 +1,7 @@
+public class Soma implements OperadoraMatematica{
+
+    @Override
+    public int soma(int a, int b) {
+        return a + b;
+    }
+}
